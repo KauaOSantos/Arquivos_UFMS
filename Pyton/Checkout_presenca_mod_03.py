83f659bbbf23b3eca9779b2455c9cc161b4a89b8
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2024-11-16 17:51:48 -0300

    fiz o Checkout de presença do mod 3 da matéria de Algoritmos e Programação I

---
+++ Pyton/Checkout_presenca_mod_03.py
@@ -0,0 +1,9 @@
+
+tempo = float(input("Digite o tempo da viagem em horas: "))
+velocidade_media = float(input("Digite a velocidade média em km/h: "))
+consumo_medio = float(input("Digite o consumo médio do veículo em km/l: "))
+
+distancia = tempo * velocidade_media
+combustivel_gasto = distancia / consumo_medio
+
+print(f"A quantidade de combustível gasto foi de {combustivel_gasto:.2f} litros.")
